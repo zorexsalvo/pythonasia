91dd1c434563187fa75d115388deef8a40c2228c
Renan Matthew Fajardo <renanmatthew206@gmail.com>
2025-11-21 01:33:04 +0800

    feat: setup the static pages w/ urls

--- app/home/urls.py
+++ app/home/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("code-of-conduct/", views.CodeOfConductView.as_view(), name="code-of-conduct"),
+    path("partnership/", views.PartnershipView.as_view(), name="partnership"),
+    path("travel-info/", views.TravelInfoView.as_view(), name="travel-info"),
+    path("aid-scholarships/", views.AidScholarshipsView.as_view(), name="aid-scholarships"),
 ]
